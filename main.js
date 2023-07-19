9545b1ca13e14fb2780daaae3ebb1fa2497be017
maknetaRo <makneta@gmail.com>
2023-07-20 00:29:45 +0200

    add few more CSS filters

--- main.js
+++ main.js
@@ -43,8 +43,85 @@ const applyBlur = () => {
 
 applyBlur();
 
+// add contrast
+const contrast = document.querySelector("#contrast");
+const applyContrast = () => {
+  contrast.addEventListener("change", (e) => {
+    const rangeValue = e.target.value;
+    context.filter = "contrast(" + rangeValue + "%)";
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applyContrast();
+
+// add grayscale()
+const grayScale = document.querySelector("#gray-scale");
+const applyGrayScale = () => {
+  grayScale.addEventListener("change", (e) => {
+    const rangeValue = e.target.value;
+    context.filter = "grayscale(" + rangeValue + "%)";
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applyGrayScale();
+
+// add invert
+const invert = document.querySelector("#invert");
+const applyInvert = () => {
+  invert.addEventListener("change", (e) => {
+    const rangeValue = e.target.value;
+    context.filter = "invert(" + rangeValue + "%)";
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applyInvert();
+
+// add saturate()
+
+const saturate = document.querySelector("#saturate");
+const applySaturate = () => {
+  saturate.addEventListener("change", (e) => {
+    const rangeValue = e.target.value;
+    context.filter = "saturate(" + rangeValue + "%)";
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applySaturate();
+
+// add sepia()
+const sepia = document.querySelector("#sepia");
+const applySepia = () => {
+  sepia.addEventListener("change", (e) => {
+    const rangeValue = e.target.value;
+    context.filter = "sepia(" + rangeValue + "%)";
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applySepia();
+
+// add opacity (?)
+const opacity = document.querySelector("#opacity");
+console.log(opacity);
+const applyOpacity = () => {
+  opacity.addEventListener("change", (e) => {
+    console.log("I'm changed");
+    const rangeValue = e.target.value;
+    console.log(rangeValue);
+    context.filter = "opacity(" + rangeValue + "%)";
+    console.log(context.filter);
+    context.drawImage(base_img, 0, 0);
+  });
+};
+
+applyOpacity();
+
 // TODO
-// add contrast(), drop-shadow(), grayscale(), hue-rotate(), invert(), saturate(), sepia(),
+//  drop-shadow(),  hue-rotate()
 // add flip vertical and horizonatal
 // make them all work together
 // scale image to fit canvas width and height
